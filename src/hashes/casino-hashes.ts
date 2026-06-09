e1c8916bf65981ca71179c3d2c1303564d8a6a3f
StarNumber12046 <starnumber.official12046@gmail.com>
2026-06-09 16:56:24 +0200

    added more hashes

--- src/hashes/casino-hashes.ts
+++ src/hashes/casino-hashes.ts
@@ -1,18 +1,26 @@
 export const KNOWN_CASINO_HASHES: string[] = [
-	// images\PXL_20260521_231846_796.jpg
+	// (existing)
 	'347871c6c9c7d8e3e38b063b34202e87347cc066c79f1c7c829b8dcd2cf4579b',
-	// images\image4.jpg
+	// (existing)
 	'25a1f12ef36e9ad11d29b1116087e0ce0d2e1ff19f70fadef04e827385813b96',
-	// images\image3.jpg
+	// (existing)
 	'777277c6c91ddb3923f327e69d0db93923b39b868c0c20be33c2c9d89c243486',
-	// images\image2.jpg
+	// (existing)
 	'121b284b793594a4b25b2c0ccd2452c3b6bbadb4f94c5a5a3da49eb096eee957',
-	// images\image.jpg
+	// (existing)
 	'8996a116743d54af16ad56a95a4be84beb4eab54e9d4c9b497b456b114a170ad',
-	// images\20260522_221131.jpg
+	// (existing)
 	'c90e6666b3a99b91ccc62c6e32fc91e98f922c6e2afe83c170306c3e9e97e609',
-	// images\1780119992747.jpg
+	// (existing)
 	'ae639e13511482ab6d5c79b6f2e9c5b6211c12cd2d3615dc5a892536d653bba9',
-	// images\1779204530006.jpg
+	// (existing)
 	'ec91038407f91f66f01ff266eeaf0d490f4622a5513eb25b8e7b96e2a191715a',
+	// images\image7.jpg
+	'4721c10ed86e3ad13f612511c48ec04e5d6e3ff1ff71f93ec04e0ce32703738d',
+	// images\image6.jpg
+	'aa3f7786e89dd939820367a38e8ddc0da27967e39f86980c2c3c22f647c2c8f8',
+	// images\image5.jpg
+	'aa85aa15554a96a5b956bab256ab4d5aaaa7715c51fa0ea789711aab551aab54',
+	// images\image.jpg
+	'ec63ad031e1c438f138e538f18c4a8733c72e970e171d6395e8cd3ad53af1e1c',
 ];
